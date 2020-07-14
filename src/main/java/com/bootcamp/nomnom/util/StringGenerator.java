8188b04f2fe879e8dfcc2bd1c3060edaec735d2d
Kristers <kristers.nikitins@inbox.lv>
2020-07-14 14:14:20 +0300

    Created method for generating a random string

--- src/main/java/com/bootcamp/nomnom/util/StringGenerator.java
+++ src/main/java/com/bootcamp/nomnom/util/StringGenerator.java
@@ -1,4 +1,15 @@
 package com.bootcamp.nomnom.util;
 
 public class StringGenerator {
+    static String getRandomString() {
+        String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder(8);
+
+        for(int i = 0; i < 8; i++) {
+            int index = (int)(symbols.length() * Math.random());
+            sb.append(symbols.charAt(index));
+        }
+
+        return sb.toString();
+    }
 }
