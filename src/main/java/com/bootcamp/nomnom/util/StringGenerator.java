68e3d8e536dcd2c503da68783c0b1b75d5e8c032
Kristers <kristers.nikitins@inbox.lv>
2020-07-14 14:43:23 +0300

    Modified method

--- src/main/java/com/bootcamp/nomnom/util/StringGenerator.java
+++ src/main/java/com/bootcamp/nomnom/util/StringGenerator.java
@@ -3,6 +3,7 @@ package com.bootcamp.nomnom.util;
 public class StringGenerator {
     static String getRandomString() {
         String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+
         StringBuilder sb = new StringBuilder(8);
 
         for(int i = 0; i < 8; i++) {
