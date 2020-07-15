5c3ec975e702dcec47015afaa896c848f806db46
Kristers <kristers.nikitins@inbox.lv>
2020-07-15 17:02:37 +0300

    Solved issue with being able to upload non-image files

--- src/main/java/com/bootcamp/nomnom/controller/RecipeController.java
+++ src/main/java/com/bootcamp/nomnom/controller/RecipeController.java
@@ -7,8 +7,11 @@ import com.bootcamp.nomnom.util.StringGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,16 +23,17 @@ public class RecipeController {
     public static String dir = RecipeController.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "../resources";
 
     @Autowired
-    RecipeRepository recipeRepository;
+    private RecipeRepository recipeRepository;
 
     /* service methods */
-    @GetMapping("/")
-    public String getAllRecipes() {
+    @GetMapping()
+    public String getAllRecipes(Model model) {
+        model.addAttribute("recipes", recipeRepository.findAll());
         return "recipes";
     }
 
     @GetMapping("/{recipeId}")
-    public String recipePage() {
+    public String recipePage(@PathVariable("recipeId") Long recipeId) {
         return "recipe-page";
     }
 
@@ -39,51 +43,53 @@ public class RecipeController {
     }
 
     @PostMapping()
-    public String postRecipe(@ModelAttribute Recipe recipe, @AuthenticationPrincipal User user, @RequestParam("file") MultipartFile file) {
+    public String postRecipe(@ModelAttribute Recipe recipe, @AuthenticationPrincipal User user, @RequestParam("file") MultipartFile file) throws IOException {
 
-        String fileName = StringGenerator.getRandomFilename(file);
-        Path filePath = Paths.get(dir + "/recipe-photos/" + fileName);
+        if(ImageIO.read(file.getInputStream()) != null) {
+            String fileName = StringGenerator.getRandomFilename(file);
+            Path filePath = Paths.get(dir + "/recipe-photos/" + fileName);
 
-        try {
-            Files.write(filePath, file.getBytes());
-            recipe.setFileName(fileName);
-            recipe.setUser(user);
-            recipeRepository.save(recipe);
-        } catch (Exception e) {
-            // need proper handling...
-            e.printStackTrace();
+            try {
+                Files.write(filePath, file.getBytes());
+                recipe.setFileName(fileName);
+                recipe.setUser(user);
+                recipeRepository.save(recipe);
+            } catch (Exception e) {
+                // need proper handling...
+                e.printStackTrace();
+                return "new-recipe";
+            }
+            // need to redirect to recipe page...
+            return "redirect:/recipe";
+
+        } else {
             return "new-recipe";
         }
 
-        // need to redirect to recipe page...
-        return "redirect:/";
     }
 
     @PostMapping("/update/{recipeId}")
-    // @pathvariable("recipeId")
-    public String updateRecipe() {
-
+    public String updateRecipe(@PathVariable("recipeId") Long recipeId) {
         return "redirect:/";
     }
 
     @GetMapping("/delete/{recipeId}")
-    // @pathvariable("recipeId")
-    public String deleteRecipe() {
+    public String deleteRecipe(@PathVariable("recipeId") Long recipeId) {
         return "redirect:/";
     }
 
     @PostMapping("/{recipeId}/comment")
-    public String addComment() {
+    public String addComment(@PathVariable("recipeId") Long recipeId) {
         return "";
     }
 
     @GetMapping("/{recipeId}/comment/delete/{commentId}")
-    public String deleteComment() {
+    public String deleteComment(@PathVariable("recipeId") Long recipeId, @PathVariable("commentId") Long commentId) {
         return "";
     }
 
     @PostMapping("/{recipeId}/like/{likeValue}")
-    public String submitLike() {
+    public String submitLike(@PathVariable("recipeId") Long recipeId, @PathVariable("likeValue") boolean recipeLike) {
         return "";
     }
 }
