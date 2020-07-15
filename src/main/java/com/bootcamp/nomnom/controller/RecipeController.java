fa7eb684d3d9fc1081a8d7599faa5c4c4f4f9519
Kristers <kristers.nikitins@inbox.lv>
2020-07-15 11:10:06 +0300

    Added filename field to recipe entity, continued create recipe method implementation

--- src/main/java/com/bootcamp/nomnom/controller/RecipeController.java
+++ src/main/java/com/bootcamp/nomnom/controller/RecipeController.java
@@ -1,20 +1,14 @@
 package com.bootcamp.nomnom.controller;
 
 import com.bootcamp.nomnom.entity.Recipe;
+import com.bootcamp.nomnom.entity.User;
 import com.bootcamp.nomnom.repository.RecipeRepository;
-import com.bootcamp.nomnom.repository.UserRepository;
 import com.bootcamp.nomnom.util.StringGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.apache.commons.io.FilenameUtils;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,63 +17,73 @@ import java.nio.file.Paths;
 @RequestMapping("/recipe")
 public class RecipeController {
 
-    // to create directory for photos, need to add lines to NomnomApplication.java before run()
-    // new File(RecipeController.dir).mkdir(); - resources folder
-    // new File(RecipeController.dir + "/recipe-photos").mkdir(); - subfolder for recipe photos
-    // new File(RecipeController.dir + "/user-photos").mkdir(); - subfolder for user photos
-    static String dir = RecipeController.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "../resources";
+    public static String dir = RecipeController.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "../resources";
 
     @Autowired
     RecipeRepository recipeRepository;
 
-    @Autowired
-    UserRepository userRepository;
+    /* service methods */
+    @GetMapping("/")
+    public String getAllRecipes() {
+        return "recipes";
+    }
 
-    /* CREATE RECIPE */
+    @GetMapping("/{recipeId}")
+    public String recipePage() {
+        return "recipe-page";
+    }
+
     @GetMapping("/new")
-    public String createRecipe(Model model) {
-        model.addAttribute("recipes", new Recipe());
-        // return thymeleaf template with creation form
+    public String createRecipe() {
+        return "new-recipe";
+    }
+
+    @PostMapping()
+    public String postRecipe(@ModelAttribute Recipe recipe, @AuthenticationPrincipal User user, @RequestParam("file") MultipartFile file) {
+
+        String fileName = StringGenerator.getRandomFilename(file);
+        Path filePath = Paths.get(dir + "/recipe-photos/" + fileName);
+
+        try {
+            Files.write(filePath, file.getBytes());
+            recipe.setFileName(fileName);
+            recipe.setUser(user);
+            recipeRepository.save(recipe);
+        } catch (Exception e) {
+            // need proper handling...
+            e.printStackTrace();
+            return "new-recipe";
+        }
+
+        // need to redirect to recipe page...
+        return "redirect:/";
+    }
+
+    @PostMapping("/update/{recipeId}")
+    // @pathvariable("recipeId")
+    public String updateRecipe() {
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{recipeId}")
+    // @pathvariable("recipeId")
+    public String deleteRecipe() {
+        return "redirect:/";
+    }
+
+    @PostMapping("/{recipeId}/comment")
+    public String addComment() {
         return "";
     }
 
-    @PostMapping("")
-    public String postRecipe(Recipe recipe, @RequestParam("file") MultipartFile file) {
-        // need to update Recipe entity - add filename column
-
-        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-        String filename = StringGenerator.getRandomString() + "." + extension;
-
-        if (extension != "jpg" || extension != "jpeg" || extension != "png") {
-            // invalid extension exception
-            return "";
-        } else {
-            Path fileNameAndPath = Paths.get(dir + "/recipe-photo/" + filename);
-
-            try {
-                Files.write(fileNameAndPath, file.getBytes());
-            } catch (IOException e) {
-                // needs proper handling
-                e.printStackTrace();
-            }
-
-            // add filename to Recipe object
-            // recipe.setFilename(filename)
-
-            try {
-                recipeRepository.save(recipe);
-            } catch (Exception e) {
-                // needs proper handling
-                e.printStackTrace();
-            }
-            // redirect to recipe list view
-            return "redirect:/";
-        }
+    @GetMapping("/{recipeId}/comment/delete/{commentId}")
+    public String deleteComment() {
+        return "";
     }
 
-    /* UPDATE RECIPE */
-
-
-    /* DELETE RECIPE */
-    
+    @PostMapping("/{recipeId}/like/{likeValue}")
+    public String submitLike() {
+        return "";
+    }
 }
