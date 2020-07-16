4cf9534ba755e9579c79044eea2a0aaeff97291f
Agate Ezera <agateezera@gmail.com>
2020-07-16 17:33:16 +0300

    User controller modification and cascades removed from Recipe entity

--- src/main/java/com/bootcamp/nomnom/controller/UserController.java
+++ src/main/java/com/bootcamp/nomnom/controller/UserController.java
@@ -1,7 +1,43 @@
 package com.bootcamp.nomnom.controller;
 
+import com.bootcamp.nomnom.entity.User;
+import com.bootcamp.nomnom.repository.UserRepository;
+import com.bootcamp.nomnom.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public String showProfilePage(Model model) {
+        return "profile";
+    }
+
+    @GetMapping("/{username}")
+    public String findUserProfile(@PathVariable("username") String username) {
+        //TODO: if the username the user is looking for matches their own username, redirect to profile page..
+        if (username.equals("currently logged in username")) {
+            return "profile";
+        }
+        return "user-page";
+    }
+
+    @PostMapping()
+    public String postImage(@ModelAttribute User user, @RequestParam("file") MultipartFile file) throws IOException {
+        userService.saveProfilePhoto(user,file);
+        return "";
+    }
+
+
 }
--- src/main/java/com/bootcamp/nomnom/service/UserService.java
+++ src/main/java/com/bootcamp/nomnom/service/UserService.java
@@ -1,12 +1,21 @@
 package com.bootcamp.nomnom.service;
 
+import com.bootcamp.nomnom.entity.Recipe;
 import com.bootcamp.nomnom.entity.User;
 import com.bootcamp.nomnom.repository.UserRepository;
+import com.bootcamp.nomnom.util.StringGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -27,4 +36,28 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
         return user;
     }
+
+    //Based on the RecipeService saveRecipe method..
+    public User saveProfilePhoto(User user, MultipartFile file) throws IOException {
+        String dir = UserService.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "../resources";
+        if (ImageIO.read(file.getInputStream()) != null) {
+            String fileName = StringGenerator.getRandomFilename(file);
+            Path filePath = Paths.get(dir + "/user-profile_photos/" + fileName);
+            try {
+                Files.write(filePath, file.getBytes());
+                user.setFileName(fileName);
+                userRepository.save(user);
+                return user;
+            } catch (Exception e) {
+                // need proper handling...
+                e.printStackTrace();
+            }
+
+        } else {
+            return null;
+        }
+        return user;
+    }
+
+
 }
