8bad64fd0078c538505c553aacb97db8c21327f2
Aelin <aelinrodriguezz@gmail.com>
2020-07-19 23:48:18 +0300

    Made simple IT example

--- src/test/java/com/bootcamp/nomnom/it/NomnomIT.java
+++ src/test/java/com/bootcamp/nomnom/it/NomnomIT.java
@@ -1,7 +1,40 @@
 package com.bootcamp.nomnom.it;
 
+import com.bootcamp.nomnom.TestData;
+import com.bootcamp.nomnom.entity.User;
+import com.bootcamp.nomnom.repository.UserRepository;
+import com.bootcamp.nomnom.service.UserService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.verify;
+
+// Here we create some simple integration tests testing random things because we promised Valdis.
 
 @SpringBootTest
 public class NomnomIT {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void createUser(){
+        User userTest = userService.saveUserRegister(TestData.getUser());
+
+        assertEquals(userTest.getUsername(), TestData.TEST_USERNAME);
+        assertNotEquals(userTest.getPassword(), TestData.TEST_PASSWORD);
+    }
+
+    @Disabled
+    @Test
+    public void saveRecipe(){
+
+    }
 }
