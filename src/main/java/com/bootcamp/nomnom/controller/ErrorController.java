3265f7818398436e300d3d359da9edab76cbb134
Aelin <aelinrodriguezz@gmail.com>
2020-07-23 03:00:07 +0300

    Made search work, added some stuffu

--- src/main/java/com/bootcamp/nomnom/controller/ErrorController.java
+++ src/main/java/com/bootcamp/nomnom/controller/ErrorController.java
@@ -1,7 +1,10 @@
 package com.bootcamp.nomnom.controller;
 
+import com.bootcamp.nomnom.entity.User;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
         return "sad-cupcake-error";
     }
 
--- src/test/java/com/bootcamp/nomnom/controller/CustomErrorControllerTest.java
+++ src/test/java/com/bootcamp/nomnom/controller/CustomErrorControllerTest.java
@@ -1,9 +1,12 @@
 package com.bootcamp.nomnom.controller;
 
+import com.bootcamp.nomnom.TestData;
+import com.bootcamp.nomnom.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,14 +15,18 @@ public class CustomErrorControllerTest {
     @InjectMocks
     CustomErrorController customErrorController;
 
+    Model model;
+    User user;
+
     @BeforeEach
     void setUp() {
+        user = TestData.getUser();
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void loginPageTest(){
-        assertEquals("sad-cupcake-error", customErrorController.handleError());
+        assertEquals("sad-cupcake-error", customErrorController.handleError(user, model));
     }
 
     @Test
